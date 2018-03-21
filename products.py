1c9728f2d13e97b57507f8e80b4d068acabd6dfa
Huang <huang37@mail2000.com.tw>
2018-03-21 19:50:50 +0800

    新增查詢檔案

--- products.py
+++ products.py
@@ -1,4 +1,21 @@
+import os
+
+# 讀取檔案
 products = []
+if os.path.isfile('products.csv'): # 檢查檔案在不在
+  print('yeah! 找到檔案了！')
+  with open('products.csv','r',encoding='utf-8') as f:
+    for line in f:
+      if '商品,價格' in line:
+        continue
+      name, price = line.strip().split(',')
+      products.append([name, price])
+  print(products)
+else:
+  print('找不到檔案。。。')
+
+
+# 讓使用者輸入
 while  True:
   name = input('請輸入商品名稱：')
   if name =='q':
@@ -7,11 +24,15 @@ while  True:
   products.append([name,pric])
 print(products)
 
+# 印出所有購買紀錄
 for p in products:
-  print(p)
+  print(p[0],'的價格是',p[1])
 
-with open('products.csv','w',encoding='utf-8') as f:
+# 寫入檔案
+with open('products.csv', 'w', encoding='utf-8') as f:
   f.write('商品,價格\n')
   for p in products:
-    f.write(p[0] + ',' + p[1] + '\n')
+    f.write(p[0] + ',' + str(p[1]) + '\n')
+
+
 
